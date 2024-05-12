82f4983ce1c3c8f3cbeb3971d4ed1c5b5be8dfdf
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-05-12 14:06:04 +0300

    => add polymorphic component - Typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import { Typography } from '@/components/ui/typography'
+
 export function App() {
-  return <div>123</div>
+  return (
+    <div>
+      <Typography.H1>H1</Typography.H1>
+      <Typography.H2>H2</Typography.H2>
+      <Typography.H3>H3</Typography.H3>
+      <Typography.H4>H4</Typography.H4>
+      <Typography.Body1>Body1</Typography.Body1>
+      <Typography.Subtitle1>Subtitle1</Typography.Subtitle1>
+      <Typography.Body2>Body2</Typography.Body2>
+      <Typography.Subtitle2>Subtitle2</Typography.Subtitle2>
+      <Typography.Caption>Caption</Typography.Caption>
+      <Typography.Overline>Overline</Typography.Overline>
+      <Typography.Link1>Link1</Typography.Link1>
+      <Typography.Link1>Link2</Typography.Link1>
+    </div>
+  )
 }
