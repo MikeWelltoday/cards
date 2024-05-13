6081980bd5dde1be9646903cbfd25f72306572d4
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-05-13 20:05:54 +0300

    => saved

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import { Typography } from '@/components/ui/typography'
+import { Dropdown } from '@/radix/dropdown/Dropdown'
 
 export function App() {
   return (
-    <div>
-      <Typography.H1>H1</Typography.H1>
-      <Typography.H2>H2</Typography.H2>
-      <Typography.H3>H3</Typography.H3>
-      <Typography.H4>H4</Typography.H4>
-      <Typography.Body1>Body1</Typography.Body1>
-      <Typography.Subtitle1>Subtitle1</Typography.Subtitle1>
-      <Typography.Body2>Body2</Typography.Body2>
-      <Typography.Subtitle2>Subtitle2</Typography.Subtitle2>
-      <Typography.Caption>Caption</Typography.Caption>
-      <Typography.Overline>Overline</Typography.Overline>
-      <Typography.Link1>Link1</Typography.Link1>
-      <Typography.Link1>Link2</Typography.Link1>
+    <div
+      style={{
+        alignItems: 'center',
+        display: 'flex',
+        height: '100vh',
+        justifyContent: 'center',
+        width: '100vw',
+      }}
+    >
+      <Dropdown />
     </div>
   )
 }
