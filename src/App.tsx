ed05cd3adcc38d4119c4c03e0282234eaaac7679
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-07-07 12:20:51 +0300

    => 🟢init

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import { TextField } from '@/components/TextField/TextField'
+
 export function App() {
   return (
     <div
       style={{
         alignItems: 'center',
         display: 'flex',
+        flexDirection: 'column',
         height: '100vh',
         justifyContent: 'center',
+        rowGap: '40px',
         width: '100vw',
       }}
     >
-      💩💩💩
+      <TextField placeholder={'Name'} type={'text'} />
+      <TextField placeholder={'Password'} type={'password'} />
     </div>
   )
 }
---
+++ src/components/TextField/TextField.tsx
@@ -0,0 +1,19 @@
+import { useId } from 'react'
+
+import s from './TextField.module.css'
+
+type TextFieldProps = {
+  placeholder: string
+  type: 'password' | 'text'
+}
+
+export const TextField = ({ placeholder, type }: TextFieldProps) => {
+  const uniqueId = useId() + 'TextField'
+
+  return (
+    <div className={s.testField}>
+      <input id={uniqueId} placeholder={placeholder} type={type} />
+      <label htmlFor={uniqueId}>{placeholder}</label>
+    </div>
+  )
+}
