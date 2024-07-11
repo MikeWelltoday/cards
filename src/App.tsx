0d2ad168d27e317b498528fa82adab95106e5f11
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-07-11 21:30:23 +0300

    => 🟢init

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { Gallery } from '@/components/Galary/Galary'
-
 export function App() {
   return (
     <div
-    // style={{
-    //   alignItems: 'center',
-    //   display: 'flex',
-    //   flexDirection: 'column',
-    //   height: '100vh',
-    //   justifyContent: 'center',
-    //   rowGap: '40px',
-    //   width: '100vw',
-    // }}
+      style={{
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+        justifyContent: 'center',
+        rowGap: '40px',
+        width: '100vw',
+      }}
     >
-      <Gallery />
+      💩💩💩
     </div>
   )
 }
